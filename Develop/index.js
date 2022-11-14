4b4cec13eab68ddba93ee472032ef6c47c34dc34
slthurman01 <slthurman01@gmail.com>
2022-11-14 09:58:33 -0600

    Adds questions, write file starting code, and two packages for application (inquirer and fs)

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,35 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
+// Array of questions for user input
 const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+questions.push("What is the title of the project?");
+questions.push("Provide a brief description of the project.");
+questions.push("Provide installation instructions for the project.");
+questions.push("Provide usage information for the project.");
+questions.push("Provide contribution guidelines for the project.");
+questions.push("Provide test instructions for the project.")
+questions.push("What is your name?");
+questions.push("What is your github username?");
+questions.push("What is your email address?");
+
+// Function to write README file
+// Sections:
+// Title, Description, Installation, Usage, Contributing, Tests, Licensing, Questions (includes name, github profile, and email address).
+
+const content = 'HERE IS THE README CONTENT';
+
+fs.writeFile('../README.md', content, err => {
+    if (err) {
+        console.error(err)
+        return
+    }
+});
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
